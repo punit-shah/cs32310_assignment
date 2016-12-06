a9bd54cf8575a597a16b349d925e6090933e53ee
Punit Shah <punit.shah@bbc.co.uk>
2016-12-06 17:35:06 +0000

    refactored translation matrix into own util function

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,22 @@
 var utils = {
+  translate: function (translateX, translateY, translateZ) {
+    return new THREE.Matrix4().makeTranslation(translateX, translateY, translateZ);
+  },
+
   translateAndRotateX: function (translateX, translateY, translateZ, rotateX) {
-    var translation = new THREE.Matrix4().makeTranslation(translateX, translateY, translateZ);
+    var translation = this.translate(translateX, translateY, translateZ);
     var rotation = new THREE.Matrix4().makeRotationX(rotateX);
     return new THREE.Matrix4().multiplyMatrices(translation, rotation);
   },
 
   translateAndRotateY: function (translateX, translateY, translateZ, rotateY) {
-    var translation = new THREE.Matrix4().makeTranslation(translateX, translateY, translateZ);
+    var translation = this.translate(translateX, translateY, translateZ);
     var rotation = new THREE.Matrix4().makeRotationY(rotateY);
     return new THREE.Matrix4().multiplyMatrices(translation, rotation);
   },
 
   translateAndRotateZ: function (translateX, translateY, translateZ, rotateZ) {
-    var translation = new THREE.Matrix4().makeTranslation(translateX, translateY, translateZ);
+    var translation = this.translate(translateX, translateY, translateZ);
     var rotation = new THREE.Matrix4().makeRotationZ(rotateZ);
     return new THREE.Matrix4().multiplyMatrices(translation, rotation);
   },
